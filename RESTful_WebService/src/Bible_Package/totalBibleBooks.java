09f5e535fca363a817dfa3915c8680fa1af98084
Fredrick Ondieki <freddysmallz@gmail.com>
2017-11-23 02:41:19 -0600

    Initial commit of the RESTful web service

---
+++ RESTful_WebService/src/Bible_Package/totalBibleBooks.java
@@ -0,0 +1,10 @@
+package Bible_Package;
+
+public class totalBibleBooks {
+	
+	public String total_BibleBooks(int Old_Testament_Books, int New_Testament_Books) {
+		return String.valueOf(Old_Testament_Books + New_Testament_Books);
+	}
+	
+
+}
