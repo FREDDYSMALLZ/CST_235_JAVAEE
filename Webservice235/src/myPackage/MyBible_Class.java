5f41c55d1364c646d3af503bf5e52f7de00dea67
Fredrick Ondieki <freddysmallz@gmail.com>
2017-11-23 01:24:08 -0600

    initial commit

---
+++ Webservice235/src/myPackage/MyBible_Class.java
@@ -0,0 +1,9 @@
+package myPackage;
+
+public class MyBible_Class {
+	public String total_BibleBooks(int Old_Testament_Books, int New_Testament_Books) {
+		return String.valueOf(Old_Testament_Books + New_Testament_Books);
+	}
+	
+
+}
